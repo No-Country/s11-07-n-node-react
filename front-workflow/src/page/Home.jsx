89d0f417d4bb4f6e84f1c7f113cb4f8c01aab41f
Nicolás <nicolud98@gmail.com>
2023-10-10 12:30:32 -0300

    Installing tailwind, routerdom, daisyUI..

---
+++ front-workflow/src/page/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="h-[100vh] w-auto flex justify-center items-center">
+      <button className="btn btn-primary">Click</button>
+    </div>
+  );
+};
+
+export default Home;
