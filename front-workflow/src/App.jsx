25f19db5e8d6e5db66138fdc877a955caec3cb23
Nicolás <nicolud98@gmail.com>
2023-10-18 12:05:23 -0300

    merge

--- front-workflow/src/App.jsx
+++ front-workflow/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<IntroCarousel />} />
+        {/* <Route path="/intro" element={<IntroCarousel />} /> */}
 
         <Route path="/home" element={<Home />} />
         {/* <Route path="/login" element={<Login />} /> */}
--- front-workflow/src/page/Home.jsx
+++ front-workflow/src/page/Home.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Carousel from "../components/homeCarousel/Carousel";
-import Login from "../components/login/Login";
 
 const Home = () => {
+  // Usuario logeado? Se queda en /
+  // Usuario no logeado y entró al sitio alguna vez? Redirecciona a /login
+  // Usuario no logeado y no entró al sitio alguna vez? Redirecciona a /intro
   return <Login />;
 };
 
