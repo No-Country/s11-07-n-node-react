c80f87824c23950b2aca1694becbac451e885177
Neider Parra <neiderpb24@gmail.com>
2023-10-19 16:34:25 -0500

    confirmacion de cambios

--- front-workflow/src/App.jsx
+++ front-workflow/src/App.jsx
@@ -1,15 +1,16 @@
 import Footer from "./components/Footer/Footer";
+import Directions from "./components/Profile/Directions";
+import EditProfile from "./components/Profile/editProfile";
+import Profile from "./components/Profile/profile";
 import Confirm from "./components/homeCarousel/Confirm";
 import WorkFlow from "./components/homeCarousel/WorkFlow";
 import Login from "./components/login/Login";
 import LoginForm from "./components/login/LoginForm";
 import LoginRegister from "./components/login/LoginRegister";
 
-
 function App() {
   return (
     <>
-      <Login/>
     </>
   );
 }
--- front-workflow/tailwind.config.js
+++ front-workflow/tailwind.config.js
@@ -1,14 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content:  ["./index.html", "./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      colors:{
+      colors: {
         verdeMarino: '#41BCAC',
         gris1: '#CFCFCF',
       }
     },
+    extend: {
+      boxShadow: {
+        'bottom-black': '0 4px 8px -2px rgba(0, 0, 0, 0.6)',
+      },
+    },
   },
   plugins: [require("daisyui")],
-  
+
 }
